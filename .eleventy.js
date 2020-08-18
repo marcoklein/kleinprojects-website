e617e4b27e7b34b670a8b6c2b641c4e5f00f0ca2
Marco Klein <marco.klein94@gmail.com>
2020-08-18 20:24:15 +0200

    structuring

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,19 @@
 const sass = require('sass');
 const fs = require('fs');
-const site = require('./_data/site.json');
 const syntaxhighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 
 // clear site on initial build
-fs.rmdirSync('_site', {recursive: true});
+fs.rmdirSync('src/_site', {recursive: true});
 console.log('Cleared _site folder');
 
 
 function compileSass() {
   console.log('Compiling sass.');
-  const cssContent = sass.renderSync({file: 'styles/main.scss'});
+  const cssContent = sass.renderSync({file: 'src/styles/main.scss'});
   fs.mkdirSync('_site/css', {recursive: true});
   fs.writeFileSync('_site/css/main.css', cssContent.css);
 }
-fs.watch('styles', (event, filepath) => {
+fs.watch('src/styles', (event, filepath) => {
   console.log('Styles file changed', filepath);
   compileSass();
 });
@@ -27,9 +26,16 @@ module.exports = function(eleventyConfig) {
 
   // Filter source file names using a glob
   eleventyConfig.addCollection("posts", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("posts/*.md").reverse();
+    return collectionApi.getFilteredByGlob("src/posts/*.md").reverse();
   });
 
   eleventyConfig.addPlugin(syntaxhighlight);
 
+  // configuration object
+  return {
+    dir: {
+      input: 'src',
+    },
+    jsDataFileSuffix: '.data',
+  };
 };
---
+++ src/_data/site.js
@@ -0,0 +1,8 @@
+/**
+ * Website relevant data like title and description.
+ */
+module.exports = {
+  // website title
+  title: "Kleinprojects",
+  subtitle: "coding projects and blog",
+}
