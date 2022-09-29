0f27090c548df0c53c644db7a30d791184377250
Marco Klein <marco.klein94@gmail.com>
2022-09-29 15:01:02 +0200

    feat: optimize playwright test setup

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,17 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
+  webServer: {
+    command: "yarn start",
+    url: "http://localhost:8080/",
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
   use: {
     headless: true,
     viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
+    baseURL: "http://localhost:8080/",
   },
 };
 export default config;
